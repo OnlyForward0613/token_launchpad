e74b28c227b5fc12b93884045cf95fc02cf8bb2c
OnlyForward0613 <harryborison613@gmail.com>
2024-01-12 08:33:15 -0800

    'fix'

--- src/components/LandingHeader/LandingHeader.tsx
+++ src/components/LandingHeader/LandingHeader.tsx
@@ -4,26 +4,28 @@ import React from 'react'
 import ConnectButton from './ConnectButton'
 import { useData } from "@/contexts/showSideBarContext";
 import Sidebar from './Sidebar';
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation'
 export default function LandingHeader() {
     const { showSideBar, setShowSideBar } = useData();
-    const router = useRouter();
+    const pathname = usePathname()
     const [pathName, setPathName] = React.useState('');
     React.useEffect(() => {
-      if (router.pathname) {
-        setPathName(router.pathname);
+      if (pathname) {
+        setPathName(pathname);
       }
-    }, [router.pathname])
+    }, [pathname])
     return (
         <div className='w-full fixed top-0 z-20'>
             <div className='px-5 md:px-[100px] py-4 md:py-6 bg-secondary-200 flex flex-col items-center justify-center w-full '>
                 <div className='max-w-[1440px] w-full flex justify-between items-center'>
+                    <Link href='/'>
                     <Image
                         src='/icons/logo.png'
                         alt='Logo Icon'
                         width={147}
                         height={39}
                     />
+                    </Link>
                     <div className='hidden md:flex text-xs lg:text-sm xl:text-base items-center text-[white] font-semibold'>
                         <Link href='/create-token' className={` ${pathName == '/create-token' && 'text-primary-200'} py-2 px-2 xl:px-4 hover:text-primary-200`}>
                             Create Token
